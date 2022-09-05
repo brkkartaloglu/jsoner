3fef832ee4278c961381a61006cb797f0a7856f1
burak <75986477+brkkartaloglu@users.noreply.github.com>
2022-09-05 10:27:57 +0300

    will be deleted

---
+++ build/precache-manifest.d2adafa855de9574b770549ce4beeb15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c0efac0e54efb6c83f043e7ec51501",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed7c0d8106bd7de509b1",
+    "url": "/static/css/main.960d731a.chunk.css"
+  },
+  {
+    "revision": "294bd895b332579a625f",
+    "url": "/static/js/2.f44bcbe3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f44bcbe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7c0d8106bd7de509b1",
+    "url": "/static/js/main.79d2eb7b.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/static/media/hero.8596ab4a.svg"
+  }
+]);
