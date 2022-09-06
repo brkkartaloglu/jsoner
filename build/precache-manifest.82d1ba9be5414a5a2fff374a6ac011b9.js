e31fdfd7844efcf989f673c8615781ecbebff9e8
burak <75986477+brkkartaloglu@users.noreply.github.com>
2022-09-06 11:13:12 +0300

    Add files via upload

---
+++ build/precache-manifest.82d1ba9be5414a5a2fff374a6ac011b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9749b4e75246c9fbdb012cd5e0863b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8c789ef63a3598716da",
+    "url": "/static/css/main.960d731a.chunk.css"
+  },
+  {
+    "revision": "71dd416443f8acb6c3c6",
+    "url": "/static/js/2.f7060552.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f7060552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c789ef63a3598716da",
+    "url": "/static/js/main.66815c4e.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/static/media/hero.8596ab4a.svg"
+  }
+]);
